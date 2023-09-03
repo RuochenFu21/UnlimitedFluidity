9c3927f19c021c8478707f4b160f52b90cd4c3e7
RuochenFu21 <mail.by.ray@gmail.com>
2023-09-03 16:22:50 +0800

    revert: revert last change

--- src/main/java/com/forsteri/unlimitedfluidity/util/Api.java
+++ src/main/java/com/forsteri/unlimitedfluidity/util/Api.java
@@ -1,10 +1,8 @@
 package com.forsteri.unlimitedfluidity.util;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
-@Documented
 @Target({ElementType.METHOD, ElementType.MODULE, ElementType.PACKAGE, ElementType.TYPE, ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.PARAMETER})
 public @interface Api {
 }
